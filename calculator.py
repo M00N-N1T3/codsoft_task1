7f9964c2796883fbf7e1bd24e9615b2bc053e324
jilanga023 <jilanga023@student.wethinkcode.co.za>
2024-06-04 09:00:12 +0200

    added the menu and added a loop entery to configure how many numbers the user would like to work with

--- calculator.py
+++ calculator.py
@@ -15,14 +15,37 @@ def menu():
 0) Exit\n\n""")
 
     while True:
-        
+
         choice = input("Choose an operation (1-4): " )
-        
-        if choice in choices:
-            print(choice)
+
+        if choice == "0":
+            exit("Thank you. Goodbye!")
+        elif choice in choices and not choice == "0":
+            return choice
         else:
             print("Please select a number between 0-5")
 
+
+def values():
+
+    while True:
+        number_of_values = input("How many numbers you want to insert")
+
+        if number_of_values.isdigit():
+            return int(number_of_values)
+
+        print("please enter real numbers only.\n")
+
+
+
+def logic():
+    operation = menu()
+    loop = values()
+
+
+
+def addition(num1, num2):
+    return sum(num1,num2)
 
 
 
