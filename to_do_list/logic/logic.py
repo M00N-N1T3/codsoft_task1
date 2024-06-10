c74cc9f8b667affc399fbe041795bc2a495779bd
jilanga023 <jilanga023@student.wethinkcode.co.za>
2024-06-10 15:29:17 +0200

    added the add function in logic, to add a new task

---
+++ to_do_list/logic/logic.py
@@ -0,0 +1,60 @@
+from os import path
+
+PRIORITIES = {
+    "o" : "Optional",
+    "l" : "Low",
+    "m" : "Medium",
+    "h" : "High",
+    "u" : "Urgent"
+}
+
+DEFAULT_NAME = "todo_list.txt"
+
+def add(name: str, description: str, mode:str, dir = DEFAULT_NAME):
+    """
+    Adds a new task to the todo list
+
+    Args:
+        dir (str): save path of the todo list [DEFAULT = todo_list.txt]
+        name (str): the name of task
+        description (str): a small description regarding the task
+        mode (str): PRIORITY of the task
+
+    Returns:
+        message : success message
+    """
+
+    HEADER = "[#] PRIORITY - NAME: DESCRIPTION\n"
+    tasks = [HEADER]
+    string = f"[{mode}] - {name}: {description}"
+
+    tmp = string.split('-')
+    task_mode = tmp[0].strip()
+    task_name = tmp[1].split(':')[0].strip()
+
+    try:
+        # first we try to read the data on the file
+        with open(dir,"r") as file:
+            tasks = file.readlines() # creating a list of all the available files
+
+    except (FileNotFoundError):
+        pass
+
+    # appending new task to list of current tasks
+    tasks.append(f'[{len(tasks)}] {string}\n');
+
+    # overwriting the file with new data
+    with open(dir,"w") as file:
+        file.writelines(tasks)
+        file.close()
+
+    return f"Successful added {task_name} to {task_mode}"
+
+def delete_task(index, file = DEFAULT_NAME):
+
+    pass
+
+
+
+message = add("test_1","works","crucial",)
+print(message)
---
+++ to_do_list/menu.py
@@ -0,0 +1,16 @@
+# a todo list
+
+# write to a file
+
+# print out the list from the file
+
+# we have optional , low, meduim, crucial and high
+
+def menu():
+
+    print("""1) show todo-list\n
+2) Add task.
+3) Modify task
+4) Delete task
+5) Complete task
+0) Quit""")
