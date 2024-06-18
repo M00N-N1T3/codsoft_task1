394f329a1706ec1c850909fd0aa456eba23074ad
jilanga023 <jilanga023@student.wethinkcode.co.za>
2024-06-18 02:10:58 +0200

    created the click command skeleton. Finished mapping the add_command

--- to_do_list/app.py
+++ to_do_list/app.py
@@ -1,4 +1,46 @@
-from lib import click as c
+from lib import click
+from logic import logic
 
-print(__import__("click"))
+
+PR_PROMPT = str(logic.PRIORITIES.values()).strip("dict_values()")
+
+@click.group()
+def main():
+    pass
+
+
+@main.command(help ="Add a new task to your todo-list")
+@click.option("-n","--name",prompt = "Enter task name.", help = "task name")
+@click.option("-d","--description", prompt ="Enter task description.",help = "task description")
+@click.option("-p","--priority", prompt ="Enter task priority " + PR_PROMPT,help = "task priority")
+@click.option("-f","--file-name",default = "",help = "to-do_list filename")
+def add(name,description,priority,file_name):
+
+    print(name)
+    if file_name == "":
+        click.echo("works")
+
+    pass
+
+@main.command()
+def update():
+    pass
+
+@main.command()
+def delete():
+    pass
+
+@main.command()
+def view():
+    pass
+
+@main.command()
+def status():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
 
