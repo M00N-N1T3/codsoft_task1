d3b196e9d9d00ea3e034fe3347ef225aa795c310
jilanga023 <jilanga023@student.wethinkcode.co.za>
2024-06-19 22:35:24 +0200

    intergrated task viewing

--- to_do_list/app.py
+++ to_do_list/app.py
@@ -1,38 +1,52 @@
 from lib import click
 from logic import logic
+from logic.os_mod import DOWNLOADS, CWD, HOME
 
 
 PR_PROMPT = str(logic.PRIORITIES.values()).strip("dict_values()")
-
 @click.group()
 def main():
     pass
 
 
 @main.command(help ="Add a new task to your todo-list")
-@click.option("-n","--name",prompt = "Enter task name.", help = "task name")
-@click.option("-d","--description", prompt ="Enter task description.",help = "task description")
-@click.option("-p","--priority", prompt ="Enter task priority " + PR_PROMPT,help = "task priority")
+@click.option("-n","--name",prompt = "Enter task name", required = True,help = "task name")
+@click.option("-d","--description", prompt ="Enter task description",help = "task description")
+@click.option("-p","--priority", prompt ="Enter task priority" + PR_PROMPT,help = "task priority")
 @click.option("-f","--file-name",default = "",help = "to-do_list filename")
 def add(name,description,priority,file_name):
 
-    print(name)
     if file_name == "":
-        click.echo("works")
+        user_input = input(f"Enter file in which to save the todo list ({logic.DEFAULT_FILENAME}): ")
+        if user_input == "":
+            file_name = logic.DEFAULT_FILENAME
+        else:
+            file_name = f"{user_input}.txt" if ".txt" not in user_input else user_input
 
-    pass
+    logic.add_task(name,description,priority,file_name)
+
 
 @main.command()
 def update():
+
     pass
 
 @main.command()
 def delete():
     pass
 
-@main.command()
-def view():
-    pass
+
+
+@main.command(help="displays your tasks")
+@click.option("-f","--filter",help="filter to display certain tasks based off of their priority",default = "ALL")
+@click.option("-p","--path",help="the path of your todo_list file",default="")
+def view(filter,path):
+
+    if path == "":
+        path = "/home/void/Desktop/project_space/CODSOFT/to_do_list/todo_list.txt"
+
+    logic.view_task(filter.upper(),path)
+
 
 @main.command()
 def status():
@@ -40,7 +54,14 @@ def status():
 
 
 if __name__ == "__main__":
-    main()
+    # print(logic.PRIORITIES.values())
+    # print(PR_PROMPT)
+    # main()
+    path = "/home/void/Desktop/project_space/CODSOFT/to_do_list/todo_list.txt"
+    logic.view_task("L",path)
+    # add()
+    pass
+
 
 
 
