62f50d6e6d8bd60b47e524754636676eded3f26b
jilanga023 <jilanga023@student.wethinkcode.co.za>
2024-06-21 01:55:25 +0200

    working on complex password pattern genration based on user requets. I have catered fo rthe case hwere a use asks just for one symbol of n length

--- password_generator/main.py
+++ password_generator/main.py
@@ -1,14 +1,14 @@
 from random import choice, randint
 from string import ascii_lowercase, ascii_uppercase
 
-numbers = "1234567890"
-symbols = "!@#$&_?"
+str_numbers = "1234567890"
+str_symbols = "!@#$&_?"
 
 
 # password_length = input("Specify the length of the password: ")
-password_length = 32
-symbol_num = 9
-up = 10
+# password_length = 32
+# symbol_num = 9
+# up = 10
 # up = input("up: ")
 # low = input("low: ")
 
@@ -20,62 +20,164 @@ up = 10
 # else:
 #     print("Please enter a real number between 0 and 32")
 
-salt = ascii_uppercase + numbers + symbols + ascii_lowercase
-s, u , l = 0, 0, 0
-password = ""
-while True:
-
-    # if len(password) != password_length and s!=symbol_num:
-    #     char = choice(salt)
-
-    #     if char in symbols and s != symbol_num:
-    #         password[randint(1,password_length)] = char
-    #         s+=1
-    # elif len(password) == password and s!=symbol_num:
-    #     # remove random char that is not a symbol
-    #     pass
-    # else:
-    #     break
-
-    if len(password) != password_length:
-        password += choice(salt)
-    else:
-        break
-
-password = list(password)
-print("old " + "".join(password))
-
-for sm in password:
-    if sm in symbols:
-        s+=1
-
-for ul in password:
-    if ul in ascii_uppercase:
-        u+=1
-
-while u!=up:
-    char = password[randint(0,password_length-1)]
-    if char not in symbols or char not in ascii_uppercase:
-        i = choice(ascii_uppercase)
-        password[password.index(char)] = i
-        u+=1
 
 
-while s!=symbol_num:
-    char = password[randint(0,password_length-1)]
-    if char not in symbols:
-        i = choice(symbols)
-        password[password.index(char)] = i
-        s+=1
+def generate_password(password_length: int):
+    salt = ascii_uppercase + str_numbers + str_symbols + ascii_lowercase
+    password = ""
+    while True:
+        if len(password) != password_length:
+            password += choice(salt)
+        else:
+            break
+    return password_length
 
-print("new " + "".join(password))
+
+
+def char_count(password,salt,):
+    count = 0
+    for char in password:
+        if char in salt:
+            count +=1
+
+    return count
 
 
 
 
-# we can have symbols / no symbols / no of symbols
-# we can have uppercase/no uppercase / no of uppercase
-# print(password)
+# password = list(password)
+# print("old " + "".join(password))
+
+# for sm in password:
+#     if sm in symbols:
+#         s+=1
+
+# for ul in password:
+#     if ul in ascii_uppercase:
+#         u+=1
+
+# while u!=up:
+#     char = password[randint(0,password_length-1)]
+#     if char not in symbols or char not in ascii_uppercase:
+#         i = choice(ascii_uppercase)
+#         password[password.index(char)] = i
+#         u+=1
+
+
+# while s!=symbol_num:
+#     char = password[randint(0,password_length-1)]
+#     if char not in symbols:
+#         i = choice(symbols)
+#         password[password.index(char)] = i
+#         s+=1
+
+# print("new " + "".join(password))
+
+def built_around_upperCase(n:int, upper_case:tuple, lower_case:tuple, numbers: tuple, symbols = tuple):
+    uFlag, upperCase_count = upper_case
+    lFlag, lowerCase_count = lower_case
+    sFlag, symbols_count = symbols
+    nFlag, numbers_count = numbers
+
+    password = generate_password(n)
+    # if no number was specified for all of them, then we assume that they just want a random one containing all
+    if upper_case == "" and lower_case == "" and numbers =="" and symbols=="":
+        return password
+
+    Up = char_count(password,ascii_uppercase)
+    Low = char_count(password,ascii_lowercase)
+    Sym = char_count(password,str_symbols)
+    Num = char_count(password,str_numbers)
+
+    # converting password to a list for mutability
+    password = list(password)
+
+    # if only specified how many uppercase characters we want and its greater than current count
+    if uFlag and not lFlag and not sFlag and not nFlag:
+
+        salt = ascii_lowercase + str_numbers + str_symbols
+        while int(upperCase_count) != Up:
+            char = password[randint(0,len(password)-1)]
+
+            if Up < int(upperCase_count):
+                if char not in ascii_uppercase:
+                    new_char = choice(ascii_uppercase) # getting a random uppercase value
+                    # password[password.index(char)] = new_char
+                    Up +=1
+            else:
+                if char in ascii_uppercase:
+                    new_char = choice(salt)
+                    Up -=1
+            password[password.index(char)] = new_char
+
+        return "".join(password)
+
+    # if they specified low
+    elif lFlag and not uFlag and not sFlag and not nFlag:
+        salt = ascii_uppercase + str_numbers + str_symbols
+        while int(lowerCase_count) != Low:
+            char = password[randint(0,len(password)-1)]
+
+            if Low < int(lowerCase_count):
+                if char not in ascii_lowercase:
+                    new_char = choice(ascii_lowercase) # getting a random uppercase value
+                    # password[password.index(char)] = new_char
+                    Low +=1
+            else:
+                if char in ascii_lowercase:
+                    new_char = choice(salt)
+                    Low -=1
+            password[password.index(char)] = new_char
+
+        return "".join(password)
+
+    # if they just specified symbols
+    elif sFlag and not uFlag and not lFlag and not nFlag:
+        salt = ascii_uppercase + str_numbers + ascii_lowercase
+        while int(symbols_count) != Sym:
+            char = password[randint(0,len(password)-1)]
+
+            if  Sym < int(symbols_count):
+                if char not in str_symbols:
+                    new_char = choice(str_symbols) # getting a random uppercase value
+                    # password[password.index(char)] = new_char
+                    Sym +=1
+            else:
+                if char in str_symbols:
+                    new_char = choice(salt)
+                    Sym -=1
+            password[password.index(char)] = new_char
+
+        return "".join(password)
+
+    # if they just specified the numbers
+    elif nFlag and not uFlag and not lFlag and not sFlag:
+        salt = ascii_uppercase + str_symbols + ascii_lowercase
+        while int(numbers_count) != Num:
+            char = password[randint(0,len(password)-1)]
+
+            if  Num < int(numbers_count):
+                if char not in str_numbers:
+                    new_char = choice(str_numbers) # getting a random uppercase value
+                    # password[password.index(char)] = new_char
+                    Sym +=1
+            else:
+                if char in str_numbers:
+                    new_char = choice(salt)
+                    Sym -=1
+            password[password.index(char)] = new_char
+
+        return "".join(password)
+
+
+# def all_the_chars(n: int):
+#     password=""
+#     salt = ascii_lowercase + ascii_uppercase + str_symbols
+#     while (len(password) != n):
+#         password += choice(salt)
+#     return password
+
+
 
 # if __name__ == "__main__":
 #     pass
