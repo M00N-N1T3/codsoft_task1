1a37eeaa2e4ba64cc6f203fb56177cd70fd90e85
jilanga023 <jilanga023@student.wethinkcode.co.za>
2024-06-28 12:17:38 +0200

    changed the color sheme to dark mode, added the functionailty for bavckspace button on the calvulaetor

--- calculator/calculators/GUI/calculator.py
+++ calculator/calculators/GUI/calculator.py
@@ -2,6 +2,11 @@
 import tkinter as tk
 
 DEFAULT_FONT = ("Arial",16)
+BLACK = "#1d1d1d"
+ORANGE = "#d34615"
+RED = "#991121"
+LIGHT_GREY = "#313131"
+WHITE = "#FFFFFF"
 
 class Calculator:
 
@@ -10,27 +15,24 @@ class Calculator:
 
         self.string = ""
         self.root = tk.Tk()
-        self.root.title("Calculator")
-        self.root.geometry("300x430")
-        self.root.minsize(width=300,height=430)
-        self.root.maxsize(width=300,height=430)
-
-
-        # self.screen.pack(padx=6,pady=6,expand=False,side=tk.TOP)
+        self.screen = Screen(self.root)
 
         self.aux_frame = tk.Frame(self.root)
+        self.aux_frame.configure(background=BLACK)
         self.screen_frame = tk.Frame(self.aux_frame)
+        self.screen_frame.configure(background=BLACK)
 
-        # self.screen = tk.Text(self.a,font=("Arial",32),width = 11,height=1,border=2)
+
         self.screen = tk.Entry(self.screen_frame,font=("Arial",32),width=11)
         self.screen.focus_set()
         self.screen.bind("<KeyPress>",self.handle_input)
 
+
         self.screen.grid(row=0,padx=3,pady=3,columnspan=5)
         self.screen_frame.pack(expand=True,fill="both")
-        # self.screen.pack(padx=6,pady=6,expand=False,fill="both")
 
         self.button_frame = tk.Frame(self.aux_frame)
+        self.button_frame.configure(background=BLACK)
 
 
         self.memory_clear = button_layout(self.button_frame,0,0,"MC")
@@ -43,7 +45,7 @@ class Calculator:
         self.clear= button_layout(self.button_frame,0,1,"C",self.clear_screen)
         self.divide = button_layout(self.button_frame,1,1,"/",lambda: self.clicked("/"))
         self.multiply= button_layout(self.button_frame,2,1,"X",lambda: self.clicked("x"))
-        self.delete= button_layout(self.button_frame,3,1,"\u2190")
+        self.delete= button_layout(self.button_frame,3,1,"\u2190",self.backspace)
 
         self.seven= button_layout(self.button_frame,0,2,"7",lambda: self.clicked("7"))
         self.eight = button_layout(self.button_frame,1,2,"8",lambda: self.clicked("8"))
@@ -62,9 +64,10 @@ class Calculator:
 
         self.percent= button_layout(self.button_frame,0,5,"%")
         self.zero= button_layout(self.button_frame,1,5,"0",lambda: self.clicked("0"))
-        self.comma= button_layout(self.button_frame,2,5,",",lambda: self.clicked(","))
+        self.comma= button_layout(self.button_frame,2,5,".",lambda: self.clicked("."))
 
         self.equal = tk.Button(self.button_frame,height=5,width=4,text="=",border=2,command= lambda: self.equals)
+        self.equal.configure(background=ORANGE)
         self.equal.grid(column=3,row=4,rowspan=2)
 
 
@@ -91,9 +94,9 @@ class Calculator:
 
         # accepted keyboard entries
         keypad = ["KP_1","KP_2","KP_3","KP_4","KP_5","KP_6","KP_7","KP_8","KP_9","KP_0",'KP_Decimal']
-        special = ["BackSpace","KP_Add","KP_Subtract","minus","plus","\u2190","Right","Left","Delete","x","slash"]
+        special = ["period","BackSpace","KP_Add","KP_Subtract","minus","plus","\u2190","Right","Left","Delete","x","slash"]
 
-        numbers = ["+",'-',",","1","2","3","4","5","6","7","8","9","0"]
+        numbers = ["+",'-',",","1","2","3","4","5","6","7","8","9","0","."]
         numbers.extend(keypad); numbers.extend(special)
 
         print(event)
@@ -109,15 +112,19 @@ class Calculator:
 
     def clicked(self,text):
         char_on_screen = self.screen.get()
-        if text != "=":
-            self.screen.insert(len(char_on_screen),text)
+        self.screen.insert(len(char_on_screen),text)
+
+    def backspace(self):
+        text = self.screen.get()
+        self.screen.delete(len(text)-1,"end")
+
+
 
     def equals(self):
         calculation = self.screen.get()
         # removing the x
         calculation = calculation.replace("x",'*') if "x" in calculation else calculation
         # removing the equal
-        
         try:
             result = str(eval(calculation))
         except Exception: # zero division error, cam mot divide by zero
@@ -137,11 +144,23 @@ class button_layout:
 
     def __init__(self,frame:tk.Frame ,column: int,row: int,text:str,command: tk.COMMAND = None,variable :tk.Variable = None):
         btn = tk.Button(frame,height=2,width=4,text=text,command=command,border=2)
+        btn.configure(background=ORANGE,activebackground=WHITE,font=("Arial",11))
         btn.grid(column=column, row=row,padx=4,pady=4)
     pass
+
+class Screen:
+
+    def __init__(self,root: tk.Tk):
+        root.title("Calculator")
+        root.configure(background=BLACK)
+        root.geometry("300x450")
+        root.minsize(width=300,height=440)
+        root.maxsize(width=300,height=430)
+
 
 
 
 if __name__ == "__main__":
     calc = Calculator()
+
 
